ac97e38abb5ed34a194fe4aee314145a7515b9ec
Samson Eze <michealexcel54@gmail.com>
2022-09-28 07:53:50 +0100

    feat!: initialized portfolio

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h2>Hi i'm</h2>
+        <h1>Samson Eze</h1>
+        <h2>a Front end Developer and Writer</h2>
+    </div>
+  )
+}
+
+export default Header
